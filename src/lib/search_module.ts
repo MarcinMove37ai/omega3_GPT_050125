a6b4c3ecde6ae2d43c553a65cf4477862521fb56
MarcinMove37ai <marcinlisiak83@gmial.com>
2025-01-09 20:23:27 +0100

    Works via Docker

--- src/lib/search_module.ts
+++ src/lib/search_module.ts
@@ -18,7 +18,7 @@ export class SearchModule {
         alpha: params.alpha
       });
 
-      const response = await fetch('http://127.0.0.1:8000/search', {
+      const response = await fetch('http://backend:8000/search', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
